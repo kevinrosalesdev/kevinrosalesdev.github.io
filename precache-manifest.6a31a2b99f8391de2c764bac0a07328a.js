45d41e412a4e7c9ca849fd415d3f5002123de931
kevinrosalesdev <kevinrosalesdev@gmail.com>
2021-06-27 00:57:48 +0200

    Updates

---
+++ precache-manifest.6a31a2b99f8391de2c764bac0a07328a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1f8d02f73e94f012929f4b917ef8c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e2851c84317729e058e",
+    "url": "/static/css/3.7e08da46.chunk.css"
+  },
+  {
+    "revision": "694cda64dd217a4865ca",
+    "url": "/static/css/4.fd3b6831.chunk.css"
+  },
+  {
+    "revision": "8aa300243a617aa47d10",
+    "url": "/static/css/main.33de3384.chunk.css"
+  },
+  {
+    "revision": "64a72302bb40089ad39b",
+    "url": "/static/js/2.1e35fcfa.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.1e35fcfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2851c84317729e058e",
+    "url": "/static/js/3.3f012192.chunk.js"
+  },
+  {
+    "revision": "694cda64dd217a4865ca",
+    "url": "/static/js/4.e9d35950.chunk.js"
+  },
+  {
+    "revision": "8aa300243a617aa47d10",
+    "url": "/static/js/main.18b8f053.chunk.js"
+  },
+  {
+    "revision": "cad389a0e4eea7be39ab",
+    "url": "/static/js/runtime-main.76bb7ef2.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "301bf35d363592915e7b37b724e4cf59",
+    "url": "/static/media/deipe.301bf35d.jpg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "36bc322b63bb49c84dcc6f28dab1313c",
+    "url": "/static/media/femepa_logo.36bc322b.png"
+  },
+  {
+    "revision": "9bbcbc5787a88a19c87069be64bfb3d0",
+    "url": "/static/media/logo-hackupc.9bbcbc57.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "8b469228128c875776f94f8d3fa51955",
+    "url": "/static/media/melbourne.8b469228.png"
+  },
+  {
+    "revision": "ab399bbd5f23b26dfd7a8fa1a8225d82",
+    "url": "/static/media/siani.ab399bbd.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "c9816344fad8c53ae4e6cb7838e9abfa",
+    "url": "/static/media/ulpgc.c9816344.png"
+  },
+  {
+    "revision": "0bcfd6b198223a870cb37113ea83bb3f",
+    "url": "/static/media/university-of-cambridge-logo.0bcfd6b1.png"
+  },
+  {
+    "revision": "9d1d729160557e2ecb3bae4ec71f9969",
+    "url": "/static/media/upc.9d1d7291.png"
+  }
+]);
